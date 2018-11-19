b9327a240c4340d15069a046d09bd5823b73c08a
vkottler <vkottler@wisc.edu>
2018-11-19 03:02:31 -0600

    re-work build exeuction and Makefile dependency tree

--- mk/outputs.mk
+++ mk/outputs.mk
@@ -1,20 +1,6 @@
-lib:  $(LIBRARY)
+lib: $(LIBRARY)
 
-TEST_BIN = $(OBJ_DIR)/test$(BUILD_FPRINT).bin
-CLIENT_BIN = $(OBJ_DIR)/client$(BUILD_FPRINT).bin
-SERVER_BIN = $(OBJ_DIR)/server$(BUILD_FPRINT).bin
-
-BINARIES += $(TEST_BIN)
-BINARIES += $(CLIENT_BIN)
-BINARIES += $(SERVER_BIN)
+run-%: $(OBJ_DIR)/%$(BUILD_FPRINT).elf
+	./$<
 
 all: $(BINARIES)
-
-server: $(SERVER_BIN)
-	./$(OBJ_DIR)/$@$(BUILD_FPRINT).elf
-
-client: $(CLIENT_BIN)
-	./$(OBJ_DIR)/$@$(BUILD_FPRINT).elf
-
-test: $(TEST_BIN)
-	./$(OBJ_DIR)/$@$(BUILD_FPRINT).elf
--- src/conf.mk
+++ src/conf.mk
@@ -3,3 +3,5 @@ include src/lib/conf.mk
 # this invocation's build configuration
 include src/arch/$(ARCH).mk
 include src/target/$(TARGET).mk
+
+include src/app/conf.mk
