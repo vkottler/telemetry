4e6915ae6940086c9dc407fae862a3052b3719e3
Mayank Katwal <nightmarewr360@aol.com>
2018-11-19 22:02:58 -0600

    manifest sends/stores, data tcp connection online

--- src/app/client.c
+++ src/app/client.c
@@ -21,17 +21,17 @@ int main(int argc, char **argv)
 	telem_manifest = channel_manifest_create(TELEMETRY_CAPACITY);
 
 	//ACCEL. CHANNELS 
-	channel_add(telem_manifest,"accel_x", "m/s^2",TELEM_UINT16,sizeof(uint16_t));
-	channel_add(telem_manifest,"accel_y", "m/s^2",TELEM_UINT16,sizeof(uint16_t));
-	channel_add(telem_manifest,"accel_z", "m/s^2",TELEM_UINT16,sizeof(uint16_t));	
+	channel_add(telem_manifest,"accel_x", "m/s^2",TELEM_UINT32,sizeof(uint32_t));
+	channel_add(telem_manifest,"accel_y", "m/s^2",TELEM_UINT32,sizeof(uint32_t));
+	channel_add(telem_manifest,"accel_z", "m/s^2",TELEM_UINT32,sizeof(uint32_t));	
 
 	//GYRO. CHANNELS 
-	channel_add(telem_manifest,"gyro_x", "deg/s",TELEM_UINT16,sizeof(uint16_t));
-	channel_add(telem_manifest,"gyro_y", "deg/s",TELEM_UINT16,sizeof(uint16_t));
-	channel_add(telem_manifest,"gyro_z", "deg/s",TELEM_UINT16,sizeof(uint16_t));
+	channel_add(telem_manifest,"gyro_x", "deg/s",TELEM_UINT32,sizeof(uint32_t));
+	channel_add(telem_manifest,"gyro_y", "deg/s",TELEM_UINT32,sizeof(uint32_t));
+	channel_add(telem_manifest,"gyro_z", "deg/s",TELEM_UINT32,sizeof(uint32_t));
 
 	//channel_print(stdout, &telem_manifest->channels[1]);
-	channel_manifest_print(stdout,telem_manifest);
+	//channel_manifest_print(stdout,telem_manifest);
 	
 	puts("Running Client: \n");
 	UNUSED(argc);
@@ -48,6 +48,7 @@ int main(int argc, char **argv)
 	server_address.sin_addr.s_addr = inet_addr("10.141.65.106");
 
 	int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	char char_buffer[256];
 	
 	//On connection, send over channel manifest if no connection error 	
 	if(connection_status == -1){
@@ -55,23 +56,60 @@ int main(int argc, char **argv)
    		return 1;
 	}
 	else {
-		printf("Connected to server\n");
+		printf("Connected to server on port 5000\n");
 		printf("Sending Manifest to server\n");
 		//Send all manifest channel information byte by byte
 		for (unsigned int i = 0; i < telem_manifest->count; i++) {
-		 	char char_buffer[256];
-			channel_t channel = telem_manifest->channels[i];
-			char_buffer[0] = channel.manifest_index; // uint32_t, 4 bytes
-			char_buffer[sizeof(uint32_t) - 1] = (channel_data_t) channel.type; // channel_data_t (enum),4 bytes
-			char_buffer[sizeof(uint32_t) + sizeof(channel_data_t) - 1] = (size_t) channel.size; // size_t, should be 4 bytes
-			sprintf(&char_buffer[sizeof(uint32_t) + sizeof(channel_data_t) + sizeof(size_t) - 1], "%s,%s\n", channel.name, channel.unit);
+			channel_t *channel = &telem_manifest->channels[i];
+			*((uint32_t *) char_buffer) = channel->manifest_index; // uint32_t, 4 bytes
+			*((channel_data_t *) &char_buffer[sizeof(uint32_t)]) = channel->type; // channel_data_t (enum),4 bytes
+			*((size_t *) &char_buffer[sizeof(uint32_t) + sizeof(channel_data_t)]) = channel->size; // size_t, should be 4 bytes
+			sprintf(&char_buffer[sizeof(uint32_t) + sizeof(channel_data_t) + sizeof(size_t)], "%s,%s\n",
+				channel->name, channel->unit);
 			
+			printf("------------Channel %d---------------\n",i);
+			printf("Channel Index: %u (%u)\n", *((uint32_t *) char_buffer), channel->manifest_index);
+			printf("Channel Type: %s\n", channel_type_to_str(*((channel_data_t *) &char_buffer[sizeof(uint32_t)])));
+			printf("Channel Size: %d \n", *((size_t *) &char_buffer[sizeof(uint32_t) + sizeof(channel_data_t)]));
+			printf("Channel Name/Unit: %s \n",(char *) &char_buffer[sizeof(uint32_t) + sizeof(channel_data_t) + sizeof(size_t)]);
+			fflush(stdout);
+
+			//Send the buffer
 		 	send(network_socket, char_buffer, sizeof(char_buffer), 0);
 	 	}
 		printf("Finished sending manifest\n");
 	}
-	
 	//Close TCP
 	close(network_socket);
+
+	//New TCP connection to streamline data 
+	int data_socket;
+	data_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	//specify address for new TCP socket
+	struct sockaddr_in data_address;
+	data_address.sin_family = AF_INET;
+	data_address.sin_port = htons(6000);
+	data_address.sin_addr.s_addr = inet_addr("10.141.65.106");
+
+	connection_status = connect(data_socket, (struct sockaddr *) &data_address, sizeof(data_address));
+	
+	//On connection, send over channel manifest if no connection error 	
+	if(connection_status == -1){
+		perror("Error making connection to socket\n");
+   		return 1;
+	}
+	else 
+	{
+		printf("Connected to server on port 6000\n");
+		printf("Sending incoming data to server\n");
+		char char_buffer[256] = {"hello"};
+		send(data_socket, char_buffer, sizeof(char_buffer), 0);
+
+
+	}
+
+	//end main
 	return 0;
+
 }
--- src/app/server.js
+++ src/app/server.js
@@ -1,25 +1,88 @@
+//import channel_data from './channel_data.js';
 
 //TCP 
 var net = require('net');
 var HOST = '10.141.65.106';
-var PORT = 5000;
+var MANIFEST_PORT = 5000;
+var DATA_PORT = 6000; 
 
+var ind_buff_size = 256; 
  
-//TCP CONNECTION SETUP
-var server = net.createServer();
-server.listen(PORT, HOST);
-console.log('listening on port: '+ PORT)
+//MANNIFEST TCP CONNECTION SETUP
+var manifest_server = net.createServer();
+manifest_server.listen(MANIFEST_PORT, HOST);
+console.log('listening on port: '+ MANIFEST_PORT)
 
-//GET INCOMING DATA 
-server.on('connection', function(sock) {
-    console.log('CONNECTED to TCP');
+//DATA server
+var data_server = net.createServer();
+
+//DATA TCP CONNECTION SETUP
+data_server.listen(DATA_PORT, HOST);
+console.log('listening on port: '+ DATA_PORT)
+
+//MANIFEST TABLE 
+var manifest_arr = [];
+
+//GET INCOMING MANIFESET DATA 
+manifest_server.on('connection', function(sock) {
+    console.log('CONNECTED to TCP on port 5000');
     sock.on('data', (data) => {
-	console.log(data)
+		var byte_len = data.byteLength
+		
+		//indexing into buffer (vaughn will throw up, need to change these in future)
+		var max_str_len = 25; 
+		var manifest_byte_offset = 4;
+		
+		console.log("Total buffer size: " + byte_len);
+		for(i = 0; i < byte_len; i += ind_buff_size)	
+		{	
+			var channel_index = data.readUInt32LE(i)  
+			var channel_type = data.readUInt32LE(i+manifest_byte_offset)
+			var channel_size = data.readUInt32LE(i+(2*manifest_byte_offset))
+			var channel_string = data.toString('utf8',i,i+max_str_len);
+			var split = channel_string.split(',');
+			var channel_name = split[0]; 
+			var channel_units = split[1];
+			var channel_object = { 
+				"index"  	:  channel_index, 
+				"data_type"	:  channel_type, 
+				"size"      :  channel_size,
+				"name"      :  channel_name,
+				"units"     :  channel_units
+			  }
+
+			manifest_arr.push(channel_object)
+		}
+		print_array(manifest_arr);
     });
-
+	sock.on('close', (data) => {
+		console.log('TCP connection closed with Manifest Server');	
+	});
 });
 
-server.on('disconnect', function() {
-  console.log('TCP connection closed');
+
+//GET INCOMING STREAM OF DATA 
+data_server.on('connection', function(sock) {
+    console.log('CONNECTED to TCP via port 6000');
+    sock.on('data', (data) => {
+		console.log('data incoming: '+ data.toString());
+	});
 });
+
+
+function print_array(arr)
+{
+	for(i = 0; i<arr.length;i++)
+	{
+		console.log("-------------------------------------------")
+		console.log("Channel Index: " + arr[i].index)
+		console.log("Channel Type: " + arr[i].data_type)
+		console.log("Channel Size: " + arr[i].size)
+		console.log("Channel Name: " + arr[i].name)
+		console.log("Channel Units: " + arr[i].units)
+	}
+
+}
+
+
 
